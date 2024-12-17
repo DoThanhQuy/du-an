abd6aa0208df3ed4e4e480595d763c920737c3d8
DoThanhQuy <thanhquydo2003@gmail.com>
2024-12-17 16:16:25 +0700

    Delete Du-An -SQL directory

--- Du-An -SQL/QuanLySinhVien.sql
+++
@@ -1,3 +0,0 @@
-SELECT [TenHP]
-FROM [dbo].[HocPhan] LEFT JOIN [dbo].[DiemHocPhan] ON [dbo].[HocPhan].MaHP = [DiemHocPhan].MaHP
-WHERE [DiemHP] > 5
--- Du-An -SQL/SQLQuery11.sql
+++
@@ -1,6 +0,0 @@
-GROUP BY[dbo].[TTSinhVien].MaSV
-
-SELECT [dbo].[TTSinhVien].MaSV, COUNT([dbo].[DiemHocPhan].DiemHP) AS SoHocPhanThieu
-FROM [dbo].[TTSinhVien] JOIN ON [dbo].[DiemHocPhan] [dbo].[dbo].[TTSinhVien].MaSV = [dbo].[DiemHocPhan].MaSV
-WHERE [DiemHP]<5 
-
